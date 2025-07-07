74c81f474fe6ae820431d8a2001492df5852672c
Madan.patakota <madan.patakota@gmail.com>
2025-07-07 09:58:53 +0530

    Rules

--- Step2/7.StaticVsNonStatic/2.Members.cs
+++ Step2/7.StaticVsNonStatic/2.Members.cs
@@ -5,8 +5,6 @@ namespace StaticAndNonStaticMembers
     public static class EmployeeStaticLogger
     {
         public static int EmployeesList = 0;
-
-
         /// <summary>
         ///  Log message is for print the Message as well as increase +1 the value
         ///  Example : Incaes EmployeesList is 2 the it will return 3
@@ -23,8 +21,12 @@ namespace StaticAndNonStaticMembers
             return EmployeesList;   //Latest emploeesList count
         }
 
-    }
+        //static EmployeeStaticLogger()
+        //{
 
+        //}
+
+    }
 
     public class EmployeeNonStaticLogger
     {
@@ -38,7 +40,7 @@ namespace StaticAndNonStaticMembers
         public void LogMessage(string message)
         {
             Console.WriteLine($"[LOG] {message}");
-            EmployeesList = EmployeesList + 1;
+            EmployeesList = EmployeesList + 1;   // 1 addition
         }
 
         public int GetMessageCount()
@@ -48,15 +50,30 @@ namespace StaticAndNonStaticMembers
 
     }
 
-
     class Program
     {
         static void Main()
         {
+            // I do't have change bcz i cannot the create the instance.
             EmployeeStaticLogger.LogMessage("John has joined on 01-10-2021");
             EmployeeStaticLogger.LogMessage("Robert has joined on 01-11-2021");
-
             Console.WriteLine($"Total static log messages: {EmployeeStaticLogger.GetMessageCount()}");
+
+
+            //Situation Based i can take any instance over here
+            EmployeeNonStaticLogger employeeNonStaticLogger1 = new EmployeeNonStaticLogger(); 
+            employeeNonStaticLogger1.LogMessage("John has joined on 01-10-2021");
+            employeeNonStaticLogger1.LogMessage("Lakshmi has joined on 02-11-2022");
+
+
+            EmployeeNonStaticLogger employeeNonStaticLogger2 = new EmployeeNonStaticLogger();
+            employeeNonStaticLogger2.LogMessage("Robert has joined on 01-11-2021");
+
+            Console.WriteLine($"Total non-static log messages: {employeeNonStaticLogger1.GetMessageCount()}");  //2
+            Console.WriteLine($"Total non-static log messages: {employeeNonStaticLogger2.GetMessageCount()}");  //1
+
+
+
 
         }
     }
---
+++ Step2/7.StaticVsNonStatic/3.LiveExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MovieNews
+{
+
+    // Note 1 : YOu cannot call the static members by using this keyboard
+    //          this always represents the Reference of the class
+
+
+
+
+
+    public class MovieDirector
+    {
+        //Static members
+
+        //static conscturo you can create only once in the staic class
+        public static string LeadActor = "Jakes";
+        public static string Cinematographer = "Fiore";
+
+        public static void CallLeadActor()
+        {
+            Console.WriteLine($"Calling Lead Actor: {MovieDirector.LeadActor}");
+        }
+
+
+        //Non-static memebers
+        public string JuniorTechnician { get; set; }
+        public string lightingAssistant { get; set; }
+
+
+        public MovieDirector(string technician, string LightingAssistant)
+        {
+            this.JuniorTechnician = technician;
+            this.lightingAssistant = LightingAssistant;
+        }
+
+        public void CallJuniorTechnician()
+        {
+            Console.WriteLine($"Calling Junior Technician: {JuniorTechnician}");
+        }
+
+        public void CallLightingAssistant()
+        {
+            Console.WriteLine($"Calling LightingAssistant: {this.lightingAssistant}");
+        }
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Direct calls
+            Console.WriteLine("Static Members (Direct Call):");
+            MovieDirector.CallLeadActor();
+            string name =  MovieDirector.Cinematographer;
+
+            // Indirect calls (Non-Static Members)
+            Console.WriteLine("\nNon-Static Members (Indirect Call):");
+            MovieDirector assistantDirector = new MovieDirector("Emily", "Clerk");
+            assistantDirector.CallJuniorTechnician();
+            assistantDirector.CallLightingAssistant();
+
+
+
+        }
+    }
+}
