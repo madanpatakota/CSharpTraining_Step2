6c130a0aff2bad60f92af83abb208ce313330812
Madan.patakota <madan.patakota@gmail.com>
2025-07-08 10:02:44 +0530

    Code commit

---
+++ Step2/10.Abstraction/Vehicle.cs
@@ -0,0 +1,90 @@
+﻿
+
+using System;
+
+namespace VehicleConfigurationSystem
+{
+
+
+    // Rule +  methodImplemention
+    abstract class Vechile
+    {
+        public string FuelType { get; set; }
+        public int Capacity { get; set; }
+
+        public abstract  void  Start();
+
+        public abstract  void  Stop();
+
+        public abstract void Engine();
+
+
+        //Concrete methods
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Fuel Type: {FuelType}");
+            Console.WriteLine($"Capacity: {Capacity} passengers");
+        }
+
+    }
+
+
+    //Derive Class  or Child Class
+
+    class Car : Vechile
+    {
+        public override  void Start()
+        {
+ 
+            Console.WriteLine("Car is starting with a key ignition.");
+        }
+
+        public override void Stop()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Car has stopped .");
+        }
+
+        public override void Engine()
+        {
+            Console.WriteLine("Car engine is a 4-cylinder petrol engine.");
+        }
+
+
+        public void CarPurcahseDateandTime()
+        {
+            Console.WriteLine("12-06-2024:12:35:00");
+        }
+
+    }
+
+
+
+    class Bike : Vechile
+    {
+        public override void Engine()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+         //   throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+
+
+}
---
+++ Step2/9.Implementation/2.TVControlSystem.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace RemotingSystem
+{
+    /// <summary>
+    /// Interface provides rules and it would't implemenet
+    /// </summary>
+
+    interface IRemote
+    {
+        void PowerOn();
+        void PowerOff();
+
+        void ChangeChannel(int channerlNumber);
+
+        void IncreaseVolume();
+
+    }
+    //I would like to take the contract of IRemote
+
+    interface IRemotePrice
+    {
+        string getPriceDetails();
+    }
+    //ctrl+.
+    
+    interface IRemoteLocationDetails
+    {
+        string getLocation();
+    }
+    
+    class SonyRemote : IRemote , IRemotePrice
+    {
+        public void ChangeChannel(int channerlNumber)
+        {
+            Console.WriteLine($"Sony TV changed to channel {channerlNumber}.");
+        }
+
+        public void IncreaseVolume()
+        {
+            Console.WriteLine("Sony TV volume increased.");
+        }
+
+        public void PowerOff()
+        {
+            Console.WriteLine("Sony TV is now Off.");
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("Sony TV is now ON.");
+        }
+    
+        public string getPriceDetails()
+        {
+            return "500Rupees";
+        }
+
+    }
+
+    class LGRemote : IRemote , IRemoteLocationDetails
+    {
+        public void ChangeChannel(int channerlNumber)
+        {
+            Console.WriteLine($"LG TV changed to channel {channerlNumber}.");
+        }
+
+        public void IncreaseVolume()
+        {
+            Console.WriteLine("LG TV volume increased.");
+        }
+
+        public void PowerOff()
+        {
+            Console.WriteLine("LG TV is now Off.");
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("LG TV is now On.");
+        }
+   
+        public string getLocation()
+        {
+            return "Bengolore";
+        }
+
+
+    }
+
+
+
+    // Before implement check the - Interface along with Class 
+    class Program
+    {
+        static void Main()
+        {
+            IRemote lgremote = new LGRemote();
+            lgremote.ChangeChannel(1);
+
+            IRemote sonyremote = new SonyRemote();
+            sonyremote.ChangeChannel(9);
+
+            IRemotePrice remotePrice = new SonyRemote();
+            string price = remotePrice.getPriceDetails();
+            Console.WriteLine(price);
+
+            IRemoteLocationDetails locationDetails = new LGRemote();
+            string location =  locationDetails.getLocation();
+            Console.WriteLine(location);
+
+        }
+    }
+
+}
+
+
+
+
